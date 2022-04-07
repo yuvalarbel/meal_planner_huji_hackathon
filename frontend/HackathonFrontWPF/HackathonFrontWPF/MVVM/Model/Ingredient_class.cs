8ca8404956191fb5a98ab9287b2422f7f191d641
OmerLif <87577356+OmerLif@users.noreply.github.com>
2022-04-07 16:00:58 +0300

    added frontend directories

---
+++ frontend/HackathonFrontWPF/HackathonFrontWPF/MVVM/Model/Ingredient_class.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_user_control
+{
+    internal class Ingredient_class
+    {
+        public string? Name { get; set; }
+        public string? Image_path { get; set; }
+        public float? Price{ get; set; }
+        public float? Quantity { get; set; }
+
+        public Ingredient_class(string? name, string? image_path, float? price, float? quantity)
+        {
+            Name = name;
+            Image_path = image_path;
+            Price = price;
+            Quantity = quantity;
+        }
+
+    }
+}
---
+++ frontend/HackathonFrontWPF/HackathonFrontWPF/MVVM/View/SingelRecipeControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace my_user_control
+{
+    /// <summary>
+    /// Interaction logic for Recipe.xaml
+    /// </summary>
+    public partial class Recipe : UserControl
+    {
+        public RecipeClass recipe { get; set; }
+        public Recipe(RecipeClass rec)
+        {
+            InitializeComponent();
+            recipe = rec;
+
+            //Adjust for current recipe
+            btn1.Content
+        }
+    }
+}
